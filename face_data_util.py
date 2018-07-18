3ccd2dcb991a5d7417bd5e94b3229e0affbbb7d6
guyf <guyfrenkel2@walla.com>
2018-07-18 20:37:54 +0300

    overfited net

--- face_data_util.py
+++ face_data_util.py
@@ -11,18 +11,18 @@ folder_names = {'train': 'FER2013Train',
                 'val': 'FER2013Valid',
                 'test': 'FER2013Test'}
 
-labels_text = 'neutral, happiness,surprise, sadness, anger, disgust, fear, contempt, unknown, NF'.split(',')
+labels_text = 'neutral,happiness,surprise,sadness,anger,disgust,fear,contempt,unknown,NF'.split(',')
 
 def get_next_batch(label_prefix_path, img_prefix_path,mode='train',batch_size=32, num_classes=7, max_sum_invalid_labels=1,
-             min_major_feeling_score=5):
+             min_major_feeling_score=5,verbose=False):
     # mode = train/val/test
     label_file_name = 'label.csv'
     f_name = folder_names[mode]
     label_path = os.path.join(label_prefix_path, f_name)
     label_path = os.path.join(label_path, label_file_name)
     img_path = os.path.join(img_prefix_path, f_name)
-
-    data = get_data(label_path,num_classes,max_sum_invalid_labels,min_major_feeling_score)
+    if verbose: print('-----'+mode+'-----')
+    data = get_data(label_path,num_classes,max_sum_invalid_labels,min_major_feeling_score,verbose)
     # shuffle the filtered data
     random.shuffle(data)
 
@@ -37,41 +37,12 @@ def get_next_batch(label_prefix_path, img_prefix_path,mode='train',batch_size=32
         batch = data[start:end]
         batch_x = [plt.imread(os.path.join(img_path, row[0])) for row in batch]
         batch_y = [row[1:] for row in batch]
-        yield batch_x,batch_y
+        yield np.array(batch_x),np.array(batch_y)
         start = end+1
 
 
 
-
-# def get_data_dicts(folder_names, label_prefix_path, img_prefix_path, num_classes=7, max_sum_invalid_labels=1,
-#              min_major_feeling_score=5):
-#     label_file_name = 'label.csv'
-#
-#     f_name = folder_names['train']
-#     label_path = os.path.join(label_prefix_path,f_name)
-#     label_path = os.path.join(label_path,label_file_name)
-#     img_path = os.path.join(img_prefix_path,f_name)
-#
-#     train_dict = get_data(label_path, img_path, num_classes, max_sum_invalid_labels, min_major_feeling_score)
-#
-#     f_name = folder_names['val']
-#     label_path = os.path.join(label_prefix_path, f_name)
-#     label_path = os.path.join(label_path, label_file_name)
-#     img_path = os.path.join(img_prefix_path, f_name)
-#
-#     val_dict = get_data(label_path, img_path, num_classes, max_sum_invalid_labels, min_major_feeling_score)
-#
-#     f_name = folder_names['test']
-#     label_path = os.path.join(label_prefix_path, f_name)
-#     label_path = os.path.join(label_path, label_file_name)
-#     img_path = os.path.join(img_prefix_path, f_name)
-#
-#     test_dict = get_data(label_path, img_path, num_classes, max_sum_invalid_labels, min_major_feeling_score)
-#
-#     return train_dict,val_dict,test_dict
-
-
-def get_data(label_path, num_classes=7, max_sum_invalid_labels=1, min_major_feeling_score=5):
+def get_data(label_path, num_classes=7, max_sum_invalid_labels=1, min_major_feeling_score=5,verbose=False):
     data = []
     with open(label_path, 'rb') as file:
         reader = csv.reader(file)
@@ -80,37 +51,19 @@ def get_data(label_path, num_classes=7, max_sum_invalid_labels=1, min_major_feel
             row = row[0:1] + map(int, row[2:])
             data.append(row)
     size_before = len(data)
-    print('before filtering there were ' + str(size_before) + ' examples')
+    if verbose:print('before filtering there were ' + str(size_before) + ' examples')
     # filter and keep only examples which contempt+unknown+NF<2 and at least one of the other feeling>5
     data = [row for row in data if
             sum(row[-3:]) <= max_sum_invalid_labels and any(i >= min_major_feeling_score for i in row[1:])]
     size_after = len(data)
-    print('after filtering there are ' + str(size_after) + ' examples')
-    print('dropped %.2f%%') % (float(size_before - size_after) / size_before * 100)
+    if verbose:print('after filtering there are ' + str(size_after) + ' examples')
+    if verbose:print('dropped %.2f%%') % (float(size_before - size_after) / size_before * 100)
     class_counters = np.zeros(num_classes)
     # statistics
     for row in data:
         ind = np.array(row[1:]).argmax()
         class_counters[ind] += 1
-    print zip(labels_text, class_counters)
+    if verbose:print zip(labels_text, class_counters)
 
     data = [row[:num_classes + 1] for row in data]
     return data
-
-    # # generate a dictionary with the image
-    # data_dict = {}
-    # for row in data:
-    #     file_name = row[0]
-    #     file_path = os.path.join(img_path, file_name)
-    #     with open(file_path, 'rb') as img_file:
-    #         image_b = img_file.read()
-    #         data_dict[file_name] = {}
-    #         data_dict[file_name]['image'] = image_b
-    #         data_dict[file_name]['labels'] = np.array(row[1:num_classes + 1])
-    # return data_dict
-
-
-
-# img_path = '/home/rocket/PycharmProjects/test/FERPlus/data_base_dir'
-# label_path = '/home/rocket/PycharmProjects/test/FERPlus/data'
-# get_data(folder_names,label_path,img_path)
--- net.py
+++ net.py
@@ -3,17 +3,36 @@ import tensorflow as tf
 import face_data_util
 import matplotlib.pyplot as plt
 import tensorflow.contrib as con
+import tensorflow.contrib.slim as slim
 import numpy as np
+import os
+from PIL import ImageFont, ImageDraw, Image
+
+tf.app.flags.DEFINE_string('inputs_path', '/home/rocket/PycharmProjects/test/FERPlus/data_base_dir',
+                           'path to input images folder')
+tf.app.flags.DEFINE_string('labels_path', '/home/rocket/PycharmProjects/test/FERPlus/data', 'path to labels folder')
+tf.app.flags.DEFINE_string('outputs_path', './outputs', 'path to program outputs folder')
+tf.app.flags.DEFINE_integer('num_classes', 7, 'number of classes to classify')
+tf.app.flags.DEFINE_integer('batch_size', 128, 'train batch size')
+tf.app.flags.DEFINE_integer('val_batch_size', 256, 'val batch size')
+tf.app.flags.DEFINE_integer('image_size', 48, 'image size')
+tf.app.flags.DEFINE_integer('max_epochs', 1000, 'num of epochs')
+tf.app.flags.DEFINE_integer('save_epoch', 3, 'save checkpoint after n epochs')
+tf.app.flags.DEFINE_float('lr_init', 1e-4, 'Initial learning rate')
+tf.app.flags.DEFINE_float('lr_decay', 0.5, 'learning rate decay factor')
+tf.app.flags.DEFINE_integer('decay_steps', 1000, 'num steps to decay lr')
+tf.app.flags.DEFINE_float('save_images', 1, 'number of epochs to save images')
+
+FLAGS = tf.app.flags.FLAGS
 
 
 class FerNet(object):
 
-    def __init__(self,num_classes=7):
-        self.num_classes=num_classes
-        self.image_size = 48
-        self.inputs = tf.placeholder(tf.float32, [None, self.image_size, self.image_size, 3], name='input')
-        self.labels = tf.placeholder(tf.float32, [None,num_classes], name='labels')
-        self.logits, self.end_points = resnet_v2_50(self.inputs, num_classes=num_classes)
+    def __init__(self):
+        self.inputs = tf.placeholder(tf.float32, [None, FLAGS.image_size, FLAGS.image_size, 1], name='input')
+        self.inputs = self.preprocess(self.inputs)
+        self.labels = tf.placeholder(tf.float32, [None, FLAGS.num_classes], name='labels')
+        self.logits, self.end_points = resnet_v2_50(self.inputs, num_classes=FLAGS.num_classes)
         self.sess = tf.InteractiveSession()
         # get the last layer before the logits
         embedding_layer = self.end_points['global_pool']
@@ -22,65 +41,230 @@ class FerNet(object):
         embedding_layer = tf.nn.l2_normalize(embedding_layer, axis=1, name='embed_normalized')
         # calc combined loss
         with tf.name_scope('loss'):
-            dml_loss = con.losses.metric_learning.triplet_semihard_loss(self.labels, embedding_layer, margin=0.5)
-            dml_loss = tf.Print(dml_loss, [dml_loss], "dml_loss: ", first_n=3, name='dml_loss')
+            max_labels = tf.argmax(self.labels, axis=1)
+            dml_loss = con.losses.metric_learning.triplet_semihard_loss(max_labels, embedding_layer, margin=0.5)
+            # dml_loss = tf.Print(dml_loss, [dml_loss], "dml_loss: ", first_n=3, summarize=50, name='dml_loss')
             cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.logits, labels=self.labels,
-                                                                    name='cross_entropy_loss')
-            cross_entropy = tf.Print(cross_entropy, [cross_entropy], "cross_entropy: ", first_n=3)
-            alpha = 1
-            beta = 1
+                                                                       name='cross_entropy_loss')
+            cross_entropy = tf.reduce_mean(cross_entropy)
+            # cross_entropy = tf.Print(cross_entropy, [cross_entropy], "cross_entropy: ", first_n=3, summarize=50)
+            alpha = 1.
+            beta = 1.
             l2_loss = tf.losses.get_regularization_loss(name='l2_loss')
-            total_loss = tf.add(alpha * dml_loss + beta * cross_entropy, l2_loss, name='total_loss')
-            self.total_loss = tf.Print(total_loss, [total_loss], "total_loss: ", first_n=3)
+            self.total_loss = tf.add(alpha * dml_loss + beta * cross_entropy, l2_loss, name='total_loss')
+            # self.total_loss = tf.Print(total_loss, [total_loss], "total_loss: ", first_n=3, summarize=50,
+            #                            name='total_loss')
 
+        pred_max = tf.argmax(self.end_points['predictions'], 1)
+        label_max = tf.argmax(self.labels, 1)
+        self.correct_predictions = tf.equal(pred_max, label_max)
         # accuracy of the trained model, between 0 (worst) and 1 (best)
-        check_predictions = tf.equal(tf.argmax(self.logits, 1), tf.argmax(self.labels, 1))
-        self.accuracy = tf.reduce_mean(tf.cast(check_predictions, tf.float32))
+        # check_predictions = tf.equal(pred_max, label_max)
+        # accuracy = tf.reduce_mean(tf.cast(check_predictions, tf.float32))
+        # self.accuracy = tf.Print(accuracy, [accuracy, self.labels, self.end_points['predictions']], 'my acc/l/p: ',
+        #                          summarize=50)
+        metric_vars_scope_name = 'my_metrics'
+        with tf.variable_scope(metric_vars_scope_name):
+            self.acc, acc_op = tf.metrics.accuracy(label_max, pred_max)
+            kappa, kappa_op = con.metrics.cohen_kappa(label_max, pred_max, FLAGS.num_classes)
+            rec, rec_op = tf.metrics.recall(label_max, pred_max)
+            pre, pre_op = tf.metrics.precision(label_max, pred_max)
+            f1 = 2 * pre * rec / (pre + rec)
+
+        self.all_metric_vars = tf.get_collection(tf.GraphKeys.LOCAL_VARIABLES, scope=metric_vars_scope_name)
+        self.metrics_init_op = tf.variables_initializer(var_list=self.all_metric_vars,
+                                                        name='validation_metrics_init')
+        self.metric_ops = tf.group(acc_op, kappa_op, rec_op, pre_op)
+
+        # confusing metrix
+        self.cm_labels = tf.placeholder(tf.int8, name='cm_labels')
+        self.cm_preds = tf.placeholder(tf.int8, name='cm_preds')
+        self.cm = tf.confusion_matrix(self.cm_labels,
+                                      self.cm_preds,
+                                      num_classes=FLAGS.num_classes, name='confusion_matrix')
+
         self.gstep = tf.Variable(0, dtype=tf.int32,
                                  trainable=False, name='global_step')
         # define the optimizer
-        self.lr = tf.Variable(1e-4, trainable=False)
-        self.train_opt = tf.train.GradientDescentOptimizer(learning_rate=self.lr).minimize(self.total_loss,
-                                                                                           global_step=self.gstep)
+        lr = tf.train.exponential_decay(FLAGS.lr_init,
+                                        self.gstep,
+                                        FLAGS.decay_steps,
+                                        FLAGS.lr_decay,
+                                        staircase=True,
+                                        name='exponential_decay_learning_rate')
+
+        self.train_opt = tf.train.AdamOptimizer(learning_rate=lr).minimize(self.total_loss,
+                                                                           global_step=self.gstep)
+
+        self.pred_max = pred_max
+        self.label_max = label_max
         # init the net
         self.sess.run(tf.global_variables_initializer())
+        self.sess.run(tf.local_variables_initializer())
 
-        self.writer_train = tf.summary.FileWriter('./graphs/train', tf.get_default_graph())
-        self.writer_test = tf.summary.FileWriter('./graphs/test', tf.get_default_graph())
+        self.train_writer = tf.summary.FileWriter(os.path.join(FLAGS.outputs_path, 'train'), tf.get_default_graph())
+        self.test_writer = tf.summary.FileWriter(os.path.join(FLAGS.outputs_path, 'test'), tf.get_default_graph())
 
-        tf.summary.scalar('accuracy', self.accuracy)
-        tf.summary.scalar('loss', self.total_loss)
+        tf.summary.scalar('Accuracy', self.acc)
+        tf.summary.scalar('Total_loss', self.total_loss)
+        tf.summary.scalar('dml_loss', dml_loss)
+        tf.summary.scalar('l2_loss', l2_loss)
+        tf.summary.scalar('cross_entropy_loss', cross_entropy)
+        tf.summary.scalar('Cohen-Kappa', kappa)
+        tf.summary.scalar('Recall', rec)
+        tf.summary.scalar('Precision', pre)
+        tf.summary.scalar('F1', f1)
+
         self.merged = tf.summary.merge_all()
-        self.saver = tf.train.Saver()
 
+        # tensor board
+        self.tb_images = tf.placeholder(tf.float32, [None, None, None, 1], name='tb_images')
+        self.img_summary = tf.summary.image('wrong_images', self.tb_images, max_outputs=50)
 
-    def train(self,batch_size=32,epochs=1):
+        self.saver = tf.train.Saver(keep_checkpoint_every_n_hours=0.5)
 
-        for epoch in range(epochs):
-            train_generator = face_data_util.get_next_batch('/home/rocket/PycharmProjects/test/FERPlus/data',
-                                                            '/home/rocket/PycharmProjects/test/FERPlus/data_base_dir',
-                                                            mode='train', batch_size=batch_size)
+    def preprocess(self, batch):
+        with tf.name_scope('preprocess'):
+            new_batch = tf.divide(batch, 255)
+            new_batch = new_batch - 0.5
+            new_batch = new_batch * 2
+        return new_batch
+
+    def get_misclassified_images(self, batch_x, batch_y):
+        preds, labels, predictions_bool = self.sess.run([self.pred_max, self.label_max, self.correct_predictions],
+                                                        feed_dict={self.inputs: batch_x, self.labels: batch_y})
+        wrong_predictions = [count for count, p in enumerate(predictions_bool) if not p]
+        space_height, space_width = 20, 140
+        images = np.zeros((len(wrong_predictions), FLAGS.image_size + space_height, FLAGS.image_size + space_width))
+        for count, index in enumerate(wrong_predictions):
+            im = np.squeeze(batch_x[index])
+            images[count][space_height:, space_width / 2:-space_width / 2] = im
+            pred = preds[index]
+            label = labels[index]
+            pil_im = Image.fromarray(images[count])
+            draw = ImageDraw.Draw(pil_im)
+            fnt = ImageFont.truetype('Pillow/Tests/fonts/FreeMono.ttf', 12)
+            pred_txt = 'P:' + face_data_util.labels_text[pred]
+            lbl_txt = 'L:' + face_data_util.labels_text[label]
+            draw.text((0, 0), pred_txt + '/' + lbl_txt, font=fnt)
+            images[count] = np.asarray(pil_im)
+        return images
+
+    def batch_loss_acc(self, feed_dict):
+        # reset the local variables, update all metrics and return the result of loss and acc
+
+        # Reset the running variables
+        self.sess.run(self.metrics_init_op)
+
+        # Update the running variables on new batch of samples
+        # save res for debugging
+        res = self.sess.run(self.metric_ops, feed_dict=feed_dict)
+
+        # Calculate the acc and loss on this batch
+        acc, loss = self.sess.run([self.acc, self.total_loss], feed_dict=feed_dict)
+
+        return loss, acc
+
+    def get_confusion_metrix(self, mode='val'):
+        generator = face_data_util.get_next_batch(FLAGS.labels_path,
+                                                  FLAGS.inputs_path,
+                                                  mode=mode, batch_size=FLAGS.val_batch_size, verbose=False)
+        gt = []
+        preds = []
+        while True:
+            try:
+                x, y = next(generator)
+                y = y / 10.
+                gt.append(y.argmax(axis=1))
+                if len(x.shape) == 3:
+                    # we need to add axis
+                    x = np.expand_dims(x, axis=3)
+            except:
+                break
+        # flatten those array
+        gt = np.array(gt).reshape(-1)
+        preds = np.array(preds).reshape(-1)
+        # get the matrix
+        matrix = self.sess.run(self.cm, feed_dict={self.cm_labels: gt, self.cm_preds: preds})
+        return matrix
+
+    def eval_net(self):
+        # validation set
+        val_generator = face_data_util.get_next_batch(FLAGS.labels_path,
+                                                      FLAGS.inputs_path,
+                                                      mode='val', batch_size=FLAGS.val_batch_size, verbose=False)
+        try:
+            val_x, val_y = next(val_generator)
+            val_y = val_y / 10.
+            if len(val_x.shape) == 3:
+                # we need to add axis
+                val_x = np.expand_dims(val_x, axis=3)
+            feed_dict = {self.inputs: val_x, self.labels: val_y}
+            loss, acc = self.batch_loss_acc(feed_dict)
+            merged = self.sess.run(self.merged, feed_dict=feed_dict)
+            return val_x, val_y, acc, loss, merged
+        except:
+            print('GOT EXCEPTION, RETURN NONE!')
+            return None
+
+    def train(self):
+        step = self.gstep.eval()
+        verbose = True
+
+        for epoch in range(FLAGS.max_epochs):
+
+            train_generator = face_data_util.get_next_batch(FLAGS.labels_path,
+                                                            FLAGS.inputs_path,
+                                                            mode='train', batch_size=FLAGS.batch_size, verbose=verbose)
+            if epoch == 0:
+                verbose = False
+            print('Epoch %d:' % epoch)
             while True:
                 try:
                     batch_x, batch_y = next(train_generator)
+                    batch_y = batch_y / 10.
+                    if len(batch_x.shape) == 3:
+                        # we need to add axis
+                        batch_x = np.expand_dims(batch_x, axis=3)
                 except:
-                    # stop iteration
-                    break;
+                    # stop ite
+                    break
 
+                # run the train operation
+                feed_dict = {self.inputs: batch_x, self.labels: batch_y}
+                self.sess.run(self.train_opt, feed_dict=feed_dict)
+                # reset and update the batch metrics and get loss and acc
+                loss, acc = self.batch_loss_acc(feed_dict)
+                # calc all the metric values for writing
+                _merged = self.sess.run(self.merged, feed_dict=feed_dict)
+                if step % 100 == 0:
+                    print('step {} - loss: {}, acc: {}'.format(step, loss, acc))
+                step += 1
+                self.train_writer.add_summary(_merged, step)
 
-
+            if (epoch % FLAGS.save_epoch) == 0:
+                self.saver.save(self.sess, FLAGS.outputs_path + '/weights/epoch', epoch)
+            # calc validation accuracy
+            val_x, val_y, acc, loss, _merged = self.eval_net()
+            self.test_writer.add_summary(_merged, step)
+            # for debugging:
+            loss_value, acc_value = self.batch_loss_acc(feed_dict={self.inputs: batch_x, self.labels: batch_y})
+            print('-----Epoch %d summary----- \nTrain - loss: %.3f, acc: %.3f\nTest - loss: %.3f, acc: %.3f' % (
+                epoch, loss_value, acc_value, loss, acc))
+            if epoch % FLAGS.save_images == 0:
+                # save wrong images to tensorboard
+                wrong_images = self.get_misclassified_images(val_x, val_y)
+                wrong_images = np.expand_dims(wrong_images, axis=3)
+                image_summary = self.sess.run(self.img_summary, feed_dict={self.tb_images: wrong_images})
+                self.train_writer.add_summary(image_summary, step)
 
     def close(self):
-        self.writer_train.close()
-        self.writer_test.close()
+        self.train_writer.close()
+        self.test_writer.close()
         self.sess.close()
 
 
-
+# main
 net = FerNet()
-
-
+net.train()
 net.close()
-
-
-
